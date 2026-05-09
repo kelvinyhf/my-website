ae3b46c7cb8ca839048e1cd9a9ae2fb699a94da1
decodr-hf <yhf719@hotmail.com>
2026-05-09 20:49:39 +0800

    style: small visual changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.addEventListener("scroll", () => {
-  const scrollThreshold = 500;
+  const scrollThreshold = 750;
   const ratio = Math.max(0, Math.min(window.scrollY / scrollThreshold, 1));
   document.documentElement.style.setProperty("--nav-opacity", ratio);
 });
